77025256341d3c2955416d8da65c95ea29bb2e52
knees <you@example.com>
2018-03-04 13:32:07 -0700

    newButtons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,22 @@
+correct = 0;
+incorrect = 0;
+unanswered = 0;
 //code runs when page is loaded
 window.onload = function() {
 	$("#timer").on("click", timer.start);
 }
 //variable holds setInterval
 	var intervalId;
+	var clockRunning = false;
+	//var inputValue = ($('input[name=bb]:checked', '#myForm').val());
+	//inputValue = parseInt(inputValue);
 //timer
 	var timer = {
 		time: 5,
 	start: function() {
-	var	intervalId = setInterval(timer.count, 1000)
-
+		if(!clockRunning)
+		intervalId = setInterval(timer.count, 1000)
+		clockRunning = true;
 	},
 	
 
@@ -18,15 +25,31 @@ window.onload = function() {
 		$("#timer").text(timer.time);
 		console.log(timer.time);
 		if (timer.time === 0) {
+			var inputValue = ($('input[name=bb]:checked', '#firstForm').val());
+			inputValue = parseInt(inputValue);
+			if (inputValue === 1) {
+				console.log(inputValue);
+			correct++
+			$("#score").text(correct);
+			//console.log($("#score"));
+} else if (inputValue === 0) {
+	incorrect--;
+	console.log(incorrect);
+}
 			//need to display scores here
-			cleatInterval(intervalId);
+			clearInterval(intervalId);
+			clockRunning = false;
 		}
 	},
 
 }
 
-$("#arnold").click(function() {
-	var inputDetect = $(this).val();
-	inputDetect = parseInt(inputDetect);
-	console.log(inputDetect);
-})
+//$("#arnold").click(function() {
+//	var inputDetect = $(this).val();
+//	inputDetect = parseInt(inputDetect);
+//	console.log(inputDetect);
+//})
+
+//$('#myForm input').on('change', function() {
+//   var inputValue = ($('input[name=bb]:checked', '#myForm').val()); 
+//});
