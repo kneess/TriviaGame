c143a6a802fd7e137647ab8202ec9a2143f158f1
knees <you@example.com>
2018-03-05 15:24:56 -0700

    valuesfrommultipleforms

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,7 +8,7 @@ window.onload = function() {
 //variable holds setInterval
 	var intervalId;
 	var clockRunning = false;
-	//var inputValue = ($('input[name=bb]:checked', '#myForm').val());
+	//var inputValue = ($('input[name=bb]:checked', '#firstForm').val());
 	//inputValue = parseInt(inputValue);
 //timer
 	var timer = {
@@ -18,38 +18,44 @@ window.onload = function() {
 		intervalId = setInterval(timer.count, 1000)
 		clockRunning = true;
 	},
-	
-
-	count: function() {
+		count: function() {
 		timer.time--;
 		$("#timer").text(timer.time);
 		console.log(timer.time);
 		if (timer.time === 0) {
-			var inputValue = ($('input[name=bb]:checked', '#firstForm').val());
-			inputValue = parseInt(inputValue);
-			if (inputValue === 1) {
-				console.log(inputValue);
-			correct++
-			$("#score").text(correct);
-			//console.log($("#score"));
-} else if (inputValue === 0) {
-	incorrect--;
-	console.log(incorrect);
-}
-			//need to display scores here
 			clearInterval(intervalId);
 			clockRunning = false;
-		}
-	},
+		if (inputValue === 1){
+		var inputValue = ($('input[name=aa]:checked', '#secondForm').val());
+		inputValue = parseInt(inputValue);
+	console.log(inputValue);
+		correct++
+		$("#score").text(correct);
+		console.log($("#score"));
+} else if (inputValue === 0) {
+	incorrect++;
+	console.log(incorrect);
 
-}
+}}
+		},
+	}
+	
+$(document).ready(function() {
+  $("#done").click(function() {
+    var answersList = [];
+    //Loop over all questoins
+    $(".firstForm").each(function() {
 
-//$("#arnold").click(function() {
-//	var inputDetect = $(this).val();
-//	inputDetect = parseInt(inputDetect);
-//	console.log(inputDetect);
-//})
+      var questionId = $(this).attr("value");
+      var answer = $("input[name='bb']:checked", $(this)).val();
 
-//$('#myForm input').on('change', function() {
-//   var inputValue = ($('input[name=bb]:checked', '#myForm').val()); 
-//});
+      //if Answer isnt provided do not update the answersList
+      if (answer !== undefined) {
+        answersList.push({
+          answer: answer
+        });
+      }
+    });
+    console.log(answersList);
+  });
+});
